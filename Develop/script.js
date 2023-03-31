eba1c73f73c6d324f64bc938186987f3601fd091
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-03-30 21:43:40 -0400

    added the day to the header

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +22,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+var rightNow = dayjs().format("dddd");
+$("#currentDay").text(rightNow);
